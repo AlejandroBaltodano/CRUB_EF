aa65f379ca8cfb8fa60883b7edb4970c285221c6
Alejandro Baltodano <carlosreyesb99@gmail.com>
2020-08-19 19:11:48 -0600

    Creacion create y lista de personas MVC

--- CRUD_EF.UI.MVC/Controllers/HomeController.cs
+++ CRUD_EF.UI.MVC/Controllers/HomeController.cs
@@ -10,8 +10,52 @@ namespace CRUD_EF.UI.MVC.Controllers
     {
         public ActionResult Index()
         {
+            AccesoADatos.Context context = new AccesoADatos.Context();
+            List<Model.PersonaModel> lalistaPersonas = context.laListaPersonas();
+
+            return View(lalistaPersonas);
+        }
+        //metodo nueva persona vista
+        public ActionResult nuevaPersona()
+        {
+
             return View();
         }
+        //metodo nuevapersona que recibe los datos de la pagina y los inserta a base de datos, usamos polimorfismo
+        [HttpPost]
+        public ActionResult nuevaPersona(Models.PersonaModelMVC nuevapersona)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    AccesoADatos.Context context = new AccesoADatos.Context();
+                    Model.PersonaModel persona = new Model.PersonaModel();
+                    persona.Nombre = nuevapersona.Nombre;
+                    persona.Cedula = nuevapersona.Cedula;
+                    persona.FechaNacimiento = nuevapersona.FechaNacimiento;
+
+                    context.insertarPersona(persona);
+
+                    //return Redirect("/");
+                    return Redirect("/");
+
+                }
+                return View(nuevapersona);
+
+               
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+
+            
+            
+        }
+
 
         public ActionResult About()
         {
---
+++ CRUD_EF.UI.MVC/Models/PersonaModelMVC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_EF.UI.MVC.Models
+{
+    public class PersonaModelMVC
+    {
+        public  int idPersona { get; set; }
+        [Required(ErrorMessage ="Debe de llenar el campo nombre completo")]
+        [Display(Name ="Nombre completo")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="Debe de llenar el campo identificacion")]
+        [Display(Name = "Identificacion")]
+        public string Cedula { get; set; }
+        
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
